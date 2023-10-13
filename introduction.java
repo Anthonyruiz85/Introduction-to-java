e50f34854446aadc814d6f3a028669b8ec15abce
Anthonyruiz85 <anthonyruiz85@outlook.com>
2023-10-13 14:50:50 -0400

    test

---
+++ introduction.java
@@ -0,0 +1,6 @@
+public class introduction {//begin 
+     public static void main(String[] args) {
+       System.out.println("Welcome to java, welcome to computer science,");//print  welcome to java, welcome to computer science
+      System.out.println(" programming is fun");//print programming is fun
+      }
+}//end
